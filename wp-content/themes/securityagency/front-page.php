bbd5c8e7ac2d4259932ff628b025a5fee6178e9a
hasnat martin <hasnat.martin@selisegroup.com>
2024-08-22 15:23:58 +0600

    homepage ready

--- wp-content/themes/securityagency/front-page.php
+++ wp-content/themes/securityagency/front-page.php
@@ -3,5 +3,8 @@
     get_template_part('templates/home_slider_template');
     get_template_part('templates/about_section_template');
     get_template_part('templates/service_template');
+    get_template_part('templates/client_template');
+    get_template_part('templates/contact_template');
+    get_template_part('templates/team_template');
     get_footer();
 ?>
